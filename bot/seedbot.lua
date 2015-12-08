a3c0780ae193a4b7f71b51bbbbc98d4e0b232d94
ryangmor <aryan.shahin@icloud.com>
2015-12-08 17:18:47 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -219,7 +219,7 @@ function create_config( )
     "download_media",
     "invite"
     },
-    sudo_users = {52038630,121189712},--Sudo users
+    sudo_users = {52038630},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
